cc8f37a9ecc9fc8ad552af912377f1113e46443c
Isabel Gutiérrez Moreno <95292801+IsabelGuMo@users.noreply.github.com>
2022-02-07 16:52:34 +0100

    initial scan page

--- src/App.js
+++ src/App.js
@@ -1,18 +1,19 @@
 import "./App.css";
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 
+
+
+import ScanPage from "./pages/ScanPage/ScanPage";
+
+
 function App() {
   return (
     <div className="App">
       <Router>
         <Routes>
-        <Route path="/" element={<HomePage />} />
-        <Route path="/login" element={<LoginPage />} />
-        <Route path="register" element={<RegisterPage />} />
-        <Route path="diary" element={<DiaryPage />} />
-        <Route path="scan" element={<ScanPage />} />
-        <Route path="scan/:id" element={<ScanDetailPage />} />
-
+         
+          <Route path="scan" element={<ScanPage />} />
+         
         </Routes>
       </Router>
     </div>
---
+++ src/pages/ScanPage/ScanPage.jsx
@@ -0,0 +1,34 @@
+import React from 'react';
+import './ScanPage.scss';
+
+const ScanPage = () => {
+    return (
+        <div className='scanPage-container'>
+        
+            <h2>Componente Escaneando...</h2>
+           
+            <p>Tan solo tienes que centrar el código de barras del producto en el recuadro</p>
+
+            <div className="imgScan-container">
+                
+            </div> 
+            <div className='scanAssets-container'>
+                <div className='code'>
+                    <img src='https://cdn.zeplin.io/5e2a11b5ca786f8064774510/assets/78F6C2BA-D0FD-4E0E-9D65-DAB96C71FFD3.png'/>
+                    <p>Codigo de barras</p>
+                </div>
+                <div className='code'>
+                    <img src='https://cdn.zeplin.io/5e2a11b5ca786f8064774510/assets/9856C95E-C5C9-4867-BD00-5458AA869E60.png'/> 
+                    <p>Codigo QR</p>
+                </div>
+                <div className='code'>
+                    <img src='https://cdn.zeplin.io/5e2a11b5ca786f8064774510/assets/80EC5F2A-CC91-43F7-9D76-1C13BE2A43B0.png'/>
+                    <p>NFC</p>
+                </div>
+            </div>   
+                   
+        </div>
+
+    );
+};
+export default ScanPage;
