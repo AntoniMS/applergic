025b0ff3a3d88c0d0d80874fc99b1244df2243ff
Sergio <pitelsergio@gmail.com>
2022-02-08 09:36:47 +0100

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,27 @@
+import "./App.scss";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import HomePage from "./pages/HomePage/HomePage";
+import LoginPage from "./pages/LoginPage/LoginPage";
+import RegisterPage from "./pages/RegisterPage/RegisterPage";
+import DiaryPage from "./pages/DiaryPage/DiaryPage";
+import ScanPage from "./pages/ScanPage/ScanPage";
+import ScanDetailPage from "./pages/ScanPage/ScanDetailPage/ScanDetailPage";
+
+function App() {
+  return (
+    <div>
+      <Router>
+        <Routes>
+          <Route path="/" element={<HomePage />} />
+          <Route path="/login" element={<LoginPage />} />
+          <Route path="register" element={<RegisterPage />} />
+          <Route path="diary" element={<DiaryPage />} />
+          <Route path="scan" element={<ScanPage />} />
+          <Route path="scan/:id" element={<ScanDetailPage />} />
+        </Routes>
+      </Router>
+    </div>
+  );
+}
+
+export default App;
